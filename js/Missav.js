6be59493ece1c91251ca7ad4f22fad69dde439de
h <168210180+18jin@users.noreply.github.com>
2024-07-25 03:04:16 +0800

    Update Missav.js

--- js/Missav.js
+++ js/Missav.js
@@ -1,16 +1,16 @@
 var rule = {
     title:'Missav',
-    host:'https://missav.com',
-    url:'/cn/fyclass/page/fypage',
+    host:'https://missav.com/dm47/cn/fc2?sort=',
+    url:'/fyclass/page/fypage',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
-    class_name:'FC2本月&FC2本周&FC2今日&FC2最新&FC2收藏数&流出本月&流出收藏数',//静态分类名称拼接
-    class_url:'fc2?sort=monthly_views&fc2?sort=weekly_views&fc2?sort=today_views&FC2&fc2?sort=saved&uncensored-leak?sort=monthly_views&uncensored-leak?sort=saved',//静态分类标识拼接
+    class_name:'FC2本月&FC2本周&FC2今日&FC2收藏数',//静态分类名称拼接
+    class_url:'monthly_views&weekly_views&today_views&saved',//静态分类标识拼接
     limit:5,
     play_parse:true,
     lazy:'',
-    一级:'.grid.grid-cols-2 div&&a;.lozad.w-full&&alt;.lozad.w-full&&data-src;.absolute.bottom-1&&Text;a&&href',
+    一级:'.l-post div&&a;a&&title;span&&data-bgsrc;.absolute.bottom-1&&Text;a&&href',
     二级:'*',
 }
