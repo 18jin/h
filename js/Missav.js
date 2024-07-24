248201bd1b77bdc4514cd0ff3d12a6a0ce418c0b
h <168210180+18jin@users.noreply.github.com>
2024-07-25 02:43:11 +0800

    Create Missav.js

---
+++ js/Missav.js
@@ -0,0 +1,17 @@
+var rule = {
+    title:'Missav',
+    host:'https://missav.com',
+	searchUrl:'/cn/search/**',
+    url:'/cn/fyclass?page=fypage',
+    headers:{
+        'User-Agent':'MOBILE_UA'
+    },
+    timeout:5000,
+    class_name:'FC2本月&FC2本周&FC2今日&FC2最新&FC2收藏数&流出本月&流出收藏数',//静态分类名称拼接
+    class_url:'fc2?sort=monthly_views&fc2?sort=weekly_views&fc2?sort=today_views&FC2&fc2?sort=saved&uncensored-leak?sort=monthly_views&uncensored-leak?sort=saved',//静态分类标识拼接
+    limit:5,
+    play_parse:true,
+    lazy:'',
+    一级:'.grid.grid-cols-2 div&&a;.lozad.w-full&&alt;.lozad.w-full&&data-src;.absolute.bottom-1&&Text;a&&href',
+    二级:'*',
+}
